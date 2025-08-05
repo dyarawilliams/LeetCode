51e97b2d6d7de12a7917bbf969d1458a1c1488fd
D'yara Williams <dyara.williams@gmail.com>
2025-08-05 19:29:23 -0400

    Time: 64 ms (21.88%), Space: 57.5 MB (84.36%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k = 0
+    for (let i = 0; i < nums.length - 2; i++){
+        if(nums[i] === nums[i+1] && nums[i] === nums[i+2]){
+            nums.splice(i, 1)
+            i--
+        }
+    }
+    return nums.length
+};
