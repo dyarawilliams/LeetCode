b47d70e53abd1a3bae6f81f74efd7fd13d8118dc
D'yara Williams <dyara.williams@gmail.com>
2025-08-05 19:45:51 -0400

    Time: 49 ms (81.02%), Space: 59.1 MB (28.12%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -5,7 +5,6 @@
 var removeDuplicates = function(nums) {
     let k = 2
     for (let i = 2; i < nums.length; i++){
-        // Check if the element to previous to current element
         if (nums[i] !== nums[k-2]){
             nums[k] = nums[i]
             k++
