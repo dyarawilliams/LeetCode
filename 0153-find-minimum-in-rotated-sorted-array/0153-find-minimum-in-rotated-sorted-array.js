6866c39eb89639dfc9a481b50a98bc65de5a7094
D'yara Williams <dyara.williams@gmail.com>
2025-08-07 14:39:05 -0400

    Time: 0 ms (100%), Space: 53.9 MB (33.73%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left = 0, right = nums.length - 1
+    let boundaryIndex = -1
+
+    while (left < right){
+        let mid = Math.floor((left + right) / 2)
+        if (nums[mid] < nums[right]){
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return nums[left]
+
+};
